a8a2fce12cfb0d7473f0a13c044589630060cdac
leey611 <leey611@newschool.edu>
2020-07-08 12:18:32 -0400

    add category model & associate

---
+++ migrations/20200708151448-add-categoryId-to-restaurant.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn('Restaurants', 'CategoryId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      regerence: {
+        model: 'Categories',
+        key: 'id'
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeColumn('Restaurants', 'CategoryId');
+  }
+};
