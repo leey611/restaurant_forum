af1f7ced9800923a8bd5d672633827754c2f0ce1
leey611 <leey611@newschool.edu>
2020-07-10 08:48:10 -0400

    feat: feeds and home

--- controllers/restControllers.js
+++ controllers/restControllers.js
@@ -67,6 +67,28 @@ let restController = {
         res.render('restaurant', { restaurant: restaurant.toJSON() });
       })
       .catch((err) => console.log(err));
+  },
+  getFeeds: (req, res) => {
+    return Restaurant.findAll({
+      limit: 10,
+      raw: true,
+      nest: true,
+      order: [['createdAt', 'DESC']],
+      include: [Category]
+    }).then((restaurants) => {
+      Comment.findAll({
+        limit: 10,
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant]
+      }).then((comments) => {
+        return res.render('feeds', {
+          restaurants: restaurants,
+          comments: comments
+        });
+      });
+    });
   }
 };
 module.exports = restController;
--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,7 @@ module.exports = (app, passport) => {
   };
 
   app.get('/', authenticated, (req, res) => res.redirect('restaurants'));
+  app.get('/restaurants/feeds', authenticated, restController.getFeeds);
   app.get('/restaurants', authenticated, restController.getRestaurants);
   app.get('/restaurants/:id', authenticated, restController.getRestaurant);
 
