e69a302f74bc0c35cb1689b408823a65a101cdf4
leey611 <leey611@newschool.edu>
2020-07-11 12:48:39 -0400

    feat: top 10 restaurants

--- controllers/restControllers.js
+++ controllers/restControllers.js
@@ -88,6 +88,23 @@ let restController = {
       })
       .catch((err) => console.log(err));
   },
+  getTopRestaurant: (req, res) => {
+    return Restaurant.findAll({
+      include: [{ model: User, as: 'FavoritedUsers' }]
+    }).then((restaurants) => {
+      restaurants = restaurants.map((r) => ({
+        ...r.dataValues,
+        FavoriteCount: r.FavoritedUsers.length,
+        isFavorited: req.user.FavoritedRestaurants.map((d) => d.id).includes(
+          r.id
+        )
+      }));
+      restaurants = restaurants
+        .sort((a, b) => b.FavoriteCount - a.FavoriteCount)
+        .slice(0, 10);
+      return res.render('topRestaurant', { restaurants });
+    });
+  },
   getFeeds: (req, res) => {
     return Restaurant.findAll({
       limit: 10,
