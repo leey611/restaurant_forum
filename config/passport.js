61ccc0922cfa7e71bdc071ce6453e120d9cd08be
leey611 <leey611@newschool.edu>
2020-07-15 17:48:10 -0400

    passport jwt authenticatedAdmin

--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,12 @@ const db = require('../models');
 const User = db.User;
 const Restaurant = db.Restaurant;
 
+// JWT
+const jwt = require('jsonwebtoken');
+const passportJWT = require('passport-jwt');
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
 // setup passport strategy
 passport.use(
   new LocalStrategy(
@@ -52,5 +58,24 @@ passport.deserializeUser((id, cb) => {
     return cb(null, user);
   });
 });
+
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = process.env.JWT_SECRET;
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  User.findByPk(jwt_payload.id, {
+    include: [
+      { model: db.Restaurant, as: 'FavoritedRestaurants' },
+      { model: db.Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  }).then((user) => {
+    if (!user) return next(null, false);
+    return next(null, user);
+  });
+});
+passport.use(strategy);
 
 module.exports = passport;
--- routes/apis.js
+++ routes/apis.js
@@ -9,20 +9,77 @@ const categoryController = require('../controllers/api/categoryController');
 const categoryService = require('../services/categoryService');
 const userController = require('../controllers/api/userController.js');
 
-router.get('/admin/restaurants', adminController.getRestaurants);
-router.get('/admin/restaurants/:id', adminController.getRestaurant);
+const passport = require('../config/passport');
+
+const authenticated = passport.authenticate('jwt', { session: false });
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) {
+      return next();
+    }
+    return res.json({ status: 'error', message: 'permission denied' });
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' });
+  }
+};
+
+router.get(
+  '/admin/restaurants',
+  authenticated,
+  authenticatedAdmin,
+  adminController.getRestaurants
+);
+router.get(
+  '/admin/restaurants/:id',
+  authenticated,
+  authenticatedAdmin,
+  adminController.getRestaurant
+);
 router.post(
   '/admin/restaurants',
+  authenticated,
+  authenticatedAdmin,
   upload.single('image'),
   adminController.postRestaurant
 );
-router.put('/admin/restaurants/:id', adminController.putRestaurant);
-router.delete('/admin/restaurants/:id', adminController.deleteRestaurant);
+router.put(
+  '/admin/restaurants/:id',
+  authenticated,
+  authenticatedAdmin,
+  adminController.putRestaurant
+);
+router.delete(
+  '/admin/restaurants/:id',
+  authenticated,
+  authenticatedAdmin,
+  adminController.deleteRestaurant
+);
 
-router.get('/admin/categories', categoryController.getCategories);
-router.post('/admin/categories', categoryService.postCategory);
-router.put('/admin/categories/:id', categoryService.putCategory);
-router.delete('/admin/categories/:id', categoryService.deleteCategory);
+router.get(
+  '/admin/categories',
+  authenticated,
+  authenticatedAdmin,
+  categoryController.getCategories
+);
+router.post(
+  '/admin/categories',
+  authenticated,
+  authenticatedAdmin,
+  categoryService.postCategory
+);
+router.put(
+  '/admin/categories/:id',
+  authenticated,
+  authenticatedAdmin,
+  categoryService.putCategory
+);
+router.delete(
+  '/admin/categories/:id',
+  authenticated,
+  authenticatedAdmin,
+  categoryService.deleteCategory
+);
 
 // JWT signin
 router.post('/signin', userController.signIn);
